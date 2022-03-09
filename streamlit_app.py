84ce040cb1e5b543c90252778f0cfee9c034fee2
Abhishika Fatehpuria <66853259+Abhishikaf@users.noreply.github.com>
2022-03-09 14:11:06 -0700

    Updated streamlit app with containers and notarization

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import streamlit as st
 import hashlib
+import pandas as pd
 
 from pinata import pin_file_to_ipfs, pin_json_to_ipfs, convert_data_to_json
 from crypto_funcs import encrypt_data, decrypt_data
@@ -61,21 +62,66 @@ def calculate_file_hash(file):
 
 # Function to get account history
 
-def get_account_history(address):
-    st.markdown("## Account history")
+def get_files_to_notarize(pending_file_container):
+
+    pending_file_container.empty()
+    pendingFiles = contract.functions.getPendingFiles().call()
+    with pending_file_container.container():
+        if pendingFiles:
+            df = pd.DataFrame(pendingFiles).T
+            df.columns = ['Document Hash', 'Owner Wallet Address']
+            st.table(df)
+
+        else:
+            st.write("No files pending notarization")
+
+
+
+
+
+def get_account_history(address, account_history):
+    
+    account_history.empty()
     document_filter = contract.events.DocumentCreated.createFilter(fromBlock="0x0", argument_filters={"docOwner": address})
     reports = document_filter.get_all_entries()
 
-    if reports:
-        for report in reports:
-            report_dictionary = dict(report)
-            st.write(report_dictionary['args']['fileHash'])
-            st.write(report_dictionary['args']['fileName'])
-            st.write(report_dictionary['args']['docType'])
-            st.write(report_dictionary['args']['status'] )
+    with account_history.container():
+        if reports:
+            for report in reports:
+                report_dictionary = dict(report)
+                html_str = f"""<p style = "line-height:50%; font-size:14px"><b> Document hash </b></p>"""
+                st.markdown(html_str, unsafe_allow_html=True)
+                st.markdown(f"""<p style = "line-height:50%; font-size:12px">{report_dictionary['args']['fileHash']}</p>""",
+                                        unsafe_allow_html=True)
+                status = contract.functions.getDocState(report_dictionary['args']['fileHash']).call()
+                if status == 0:
+                    file_status = "Not Notarized"
+                elif status == 1:
+                    file_status = "Pending Notarization "
+                elif status == 2:
+                    file_status = "Notarized"
+                         
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.markdown(f"""<p style = "line-height:50%; font-size:12px"><b> File name :</b></p>""",
+                                        unsafe_allow_html=True ) 
+                    st.markdown(f"""<p style = "line-height:50%; font-size:12px"><b> File Desc :</b></p>""",
+                                        unsafe_allow_html=True ) 
+                    st.markdown(f"""<p style = "line-height:50%; font-size:12px"><b> Status :</b></p>""",
+                                        unsafe_allow_html=True ) 
+                
+                with col2:
+                    st.markdown(f"""<p style = "line-height:50%; font-size:12px">{report_dictionary['args']['fileName']}</p>""",
+                                        unsafe_allow_html=True ) 
+                    st.markdown(f"""<p style = "line-height:50%; font-size:12px">{report_dictionary['args']['docType']}</p>""",
+                                        unsafe_allow_html=True ) 
+                    st.markdown(f"""<p style = "line-height:50%; font-size:12px">{file_status}</p>""",
+                                        unsafe_allow_html=True ) 
+                
+                st.markdown(f"""<p style = "line-height:50%; font-size:12px">---</p>""",unsafe_allow_html=True)
         
-    else:
-        st.write("This account has not uploaded any documents")
+        else:
+            st.write("This account has not uploaded any documents")
 
 
 ################################################################################
@@ -129,22 +175,28 @@ page = st.sidebar.radio('', options=['Client Register and File Selection', 'Nota
 
 st.sidebar.markdown("""---""")
 
+accounts = w3.eth.accounts
+
+
+
 if page == 'Client Register and File Selection':
+    
+    
     doEncrypt=False
 
     st.markdown("## Register Client and Select File")
     st.write("Choose an account to get started")
-    accounts = w3.eth.accounts
-    address = st.selectbox("Select Account", options=accounts)
+    
+    address = st.selectbox("Select Account", options=accounts[0:8])
     st.markdown("---")
 
-    # email_addr = st.text_input("Please register with email address:")
-    # st.write("Email entered:", email_addr)
-#    registered = contract.functions.newClient(email_addr)
-    # Save for use on other pages
-    # st.session_state.client = email_addr
+    st.sidebar.markdown("### Client Wallet Address:")
+    st.sidebar.markdown(f"""<p style = "line-height:50%; font-size:14px">{address}</p>""",unsafe_allow_html=True)
+    st.sidebar.markdown("### Account History")
 
-    
+    account_history = st.sidebar.empty()
+    get_account_history(address, account_history)
+
     doc_type = st.text_input("Enter the type of the document to be uploaded")
     st.write(" Document Type:", doc_type)
     st.session_state.doc_type = doc_type
@@ -166,20 +218,18 @@ if page == 'Client Register and File Selection':
                 st.session_state.password = password
 
         doNotarize = st.checkbox("Notarize " + file.name )
-        # if doNotarize:
-        #     notary = st.selectbox("Select a Notary:", notaries)
-        #     st.session_state.notary = notary
-        #     st.write("Selected:", notary)
-
-        
+                
         if st.button("Submit"):   
             doc_hash = calculate_file_hash(file)
             st.session_state.doc_hash = doc_hash
-            file_ipfs_hash = pin_file(
-                st.session_state.file.name,
-                st.session_state.file,
-                password
-             )  
+            if(not doNotarize):
+                file_ipfs_hash = pin_file(
+                    st.session_state.file.name,
+                    st.session_state.file,
+                    password
+                )
+            else:
+                file_ipfs_hash = ""  
             try:
                 tx_hash = contract.functions.createDoc(file.name, doc_type, 
                                                     address, doc_hash,
@@ -188,36 +238,63 @@ if page == 'Client Register and File Selection':
                 receipt = w3.eth.waitForTransactionReceipt(tx_hash)
                 st.write("Transaction receipt mined:")            
             except:
-                st.write("File is alreay saved on the blockchain!")
+                 st.write("File is alreay saved on the blockchain!")
     
 
-get_account_history(address)
+    get_account_history(address, account_history)
+
+
 
 if page == 'Notary Signature':
     st.title("Notarize File")
-    st.write("Notary chosen:", st.session_state.notary)
-    st.write("Client email:", st.session_state.client)
-    st.write("File name:", st.session_state.file.name)
-    if 'password' in st.session_state:
-        st.write("Do encrypt:", st.session_state.password)
-        password = st.session_state.password
-    else:
-        password = ""
 
-    if st.button("Confirm and notarize"):
-        file_ipfs_hash = pin_file(
-            st.session_state.file.name,
-            st.session_state.file,
-            st.session_state.notary,
-            password
-            )
+    st.markdown("### Files Pending Notarization")
 
-        # If we want to display link to file itself, we need pin_file() to return ipfs_file_hash
-        #   Otherwise, that ipfs_file_hash can be read from the metadata
-        st.write("IPFS Gateway Link to notarized file metadata:")
-        st.markdown(f"[Pinned metadata for notarized file](https://ipfs.io/ipfs/{file_ipfs_hash})")
+    pending_file_container = st.empty()
+
+    get_files_to_notarize(pending_file_container)
+
+
+    notary = st.selectbox("Select Notary Address", options = accounts[8:10])
+    license = st.text_input("Notary License Number")
+    doc_hash = st.text_input("Document Hash")
+    address = st.text_input("Owner address")
+    if(st.button("Notarize")):
+        st.write(notary, license,doc_hash, address)
+        try:
+            tx_hash = contract.functions.notarizeDoc(notary, license, doc_hash     
+                                                ).transact({'from': address, 'gas': 1000000})
+            receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+            st.write("Transaction receipt mined:")            
+        except:
+            st.write("File is not pending notarization")
+
+    get_files_to_notarize(pending_file_container)
+
+    # st.markdown("## Files Notarized")
+    # st.write("Notary chosen:", st.session_state.notary)
+    # st.write("Client email:", st.session_state.client)
+    # st.write("File name:", st.session_state.file.name)
+    # if 'password' in st.session_state:
+    #     st.write("Do encrypt:", st.session_state.password)
+    #     password = st.session_state.password
+    # else:
+    #     password = ""
+
+    # if st.button("Confirm and notarize"):
+    #     file_ipfs_hash = pin_file(
+    #         st.session_state.file.name,
+    #         st.session_state.file,
+    #         st.session_state.notary,
+    #         password
+    #         )
+
+    #     # If we want to display link to file itself, we need pin_file() to return ipfs_file_hash
+    #     #   Otherwise, that ipfs_file_hash can be read from the metadata
+    #     st.write("IPFS Gateway Link to notarized file metadata:")
+    #     st.markdown(f"[Pinned metadata for notarized file](https://ipfs.io/ipfs/{file_ipfs_hash})")
         # contract.functions.notarizeFile(...)
 
-if page == 'Verification':
-    st.title("Verify Authenticity")
+# if page == 'Verification':
+#     st.title("Verify Authenticity")
 
